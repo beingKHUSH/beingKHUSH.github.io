3e211a189db7d174de74fefe81bd35bb8ccb125d
Khushboo Jain <30721450+beingKHUSH@users.noreply.github.com>
2018-07-02 00:34:25 +0530

    edited

--- js/function.js
+++ js/function.js
@@ -2,8 +2,11 @@
 function openSlideMenu(){
   document.getElementById('sidebar').style.width = '250px';
   document.getElementById('main').style.marginLeft = '250px';
+  document.getElementById('sidebarCollapse').style.display = "none";
+
 }
 function closeSlideMenu(){
   document.getElementById('sidebar').style.width = '0';
   document.getElementById('main').style.marginLeft = '0';
+  document.getElementById('sidebarCollapse').style.display = "block";
 }
